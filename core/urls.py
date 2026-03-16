55136f475e8b4f35985bfd5fc2f7199fff388d30
Antony Nyagah <tony.m.nyagah@gmail.com>
2026-03-16 15:44:01 +0300

    feat: add a proper api root view

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
+
+class APIRootView(APIView):
+    """
+    # Django Query Optimization Practice
+
+    Welcome! This API exposes endpoints that demonstrate optimized and
+    unoptimized Django ORM queries.
+
+    Browse the available endpoints below.
+    """
+
+    def get(self, request):
+        return Response(
+            {
+                "users": reverse("users-list", request=request),
+            }
+        )
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", APIRootView.as_view(), name="api-root"),
     path("api/", include("users.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
