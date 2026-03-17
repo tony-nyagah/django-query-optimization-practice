b178d60ef0c9ddb1f8ea62dad18c213fc0914544
Antony Nyagah <tony.m.nyagah@gmail.com>
2026-03-17 20:22:12 +0300

    Load DEBUG from environment
    Make settings DEBUG depend on the DEBUG environment variable (defaults
    to
    False) and add DEBUG: "False" to docker-compose so containers run with a
    non-debug default

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.token_hex(32)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,.nyagah.me").split(
     ","
